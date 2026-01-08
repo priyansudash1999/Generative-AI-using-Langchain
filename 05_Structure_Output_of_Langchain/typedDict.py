b58017749cf72f1a1b1a2fc4651f3713ad4149a5
priyansudash1999 <1999priyansudash@gmail.com>
2026-01-08 15:11:11 +0530

    literal typedDict

--- 05_Structure_Output_of_Langchain/typedDict.py
+++ 05_Structure_Output_of_Langchain/typedDict.py
@@ -1,4 +1,4 @@
-from typing import TypedDict, Annotated, Optional
+from typing import TypedDict, Annotated, Optional, Literal
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
@@ -13,7 +13,7 @@ model = ChatOpenAI(model="gpt-4.1-nano", temperature=0)
 class Review(TypedDict):
   key_themes: Annotated[str, "Write down all the key themes discussed in the review of the list"]
   summary: Annotated[str, "A brief summary of the review"]
-  sentiment: Annotated[str, "The sentiment of the review either positive or negative or neutral"]
+  sentiment: Annotated[Literal["pos", "neg"], "The sentiment of the review either positive or negative or neutral"]
   pros: Annotated[Optional[list[str]], "Write down all the pros inside the list"]
   cons: Annotated[Optional[list[str]], "Write down all the cons inside the list"]
 
@@ -21,3 +21,4 @@ str_model = model.with_structured_output(Review)
 result = str_model.invoke(""" I recently used the Realme 12 Pro+, and unfortunately, my experience was disappointing. While the phone boasts attractive specs on paper, it fell short in several key areas. The software felt unstable, with frequent lags and inconsistent performance during everyday tasks. Battery life was mediocre at best, barely lasting a full day with moderate use. The camera, despite its high megapixel count, struggled in low-light conditions and produced washed-out colors. Additionally, the build quality didn’t feel as premium as expected for a device in this price range. All things considered, I expected much better, but the Realme 12 Pro+ ultimately left me underwhelmed.""")
 
 print(result)
+print(result["sentiment"])
